15d16ce3917a20b891a1560ab50a6927be90079c
Javohir Nurmatjonov <javoxirone@gmail.com>
2021-06-26 19:57:38 +0500

    added multilanguage

---
+++ js/lang.js
@@ -0,0 +1,254 @@
+const langArr = {
+    "unit" : {
+        "en": "we sell milking machines",
+        "ru": "продаем доительные машины",
+        "uz": "sog'uvchi uskunalar sotamiz"
+    },
+    "home" : {
+        "en": "Home",
+        "ru": "Главная",
+        "uz": "Asosiy"
+    },
+    "hiw" : {
+        "en": "How it works",
+        "ru": "Как это работает",
+        "uz": "Bu qanday ishlaydi"
+    },
+    "gallery" : {
+        "en": "Gallery",
+        "ru": "Галерея",
+        "uz": "Galereya"
+    },
+    "testimonials" : {
+        "en": "Testimonials",
+        "ru": "Отзывы",
+        "uz": "Fikrlar"
+    },
+    "join" : {
+        "en": "JOIN",
+        "ru": "ПРИСОЕДИНИТЬСЯ",
+        "uz": "QO'SHLISH"
+    },
+    "copyright" : {
+        "en": "© 2020 MilkStar. Made With Love by JavoxirOne",
+        "ru": "© 2020 MilkStar. Сделано с любовью JavoxirOne",
+        "uz": "© 2020 MilkStar. Mehr bilan yaratilgan JavoxirOne"
+    },
+    "home-a" : {
+        "en": "Home",
+        "ru": "Главная",
+        "uz": "Asosiy"
+    },
+    "hiw-a" : {
+        "en": "How it works",
+        "ru": "Как это работает",
+        "uz": "Bu qanday ishlaydi"
+    },
+    "gallery-a" : {
+        "en": "Gallery",
+        "ru": "Галерея",
+        "uz": "Galereya"
+    },
+    "testimonials-a" : {
+        "en": "Testimonials",
+        "ru": "Отзывы",
+        "uz": "Fikrlar"
+    },
+    "join-a" : {
+        "en": "JOIN",
+        "ru": "ПРИСОЕДИНИТЬСЯ",
+        "uz": "QO'SHLISH"
+    },
+    "hiw-b" : {
+        "en": "How it works",
+        "ru": "Как это работает",
+        "uz": "Bu qanday ishlaydi?"
+    },
+    "gallery-b" : {
+        "en": "Gallery",
+        "ru": "Галерея",
+        "uz": "Galereya"
+    },
+    "testimonials-b" : {
+        "en": "Testimonials",
+        "ru": "Отзывы",
+        "uz": "Fikrlar"
+    },
+    "home-title" : {
+        "en": "The best dairy machines in Uzbekistan",
+        "ru": "Лучшие молочные машины в Узбекистане",
+        "uz": "O'zbekistondagi eng yaxshi sog'uvchi mashinalar"
+    },
+    "btn-main" : {
+        "en": "Join With Us!",
+        "ru": "Присоединиться!",
+        "uz": "Qoshilish!"
+    },
+    "btn-main-border" : {
+        "en": "Learn More!",
+        "ru": "Подробнее",
+        "uz": "Batafsil"
+    },
+    "btn-main-border-a" : {
+        "en": "Learn More!",
+        "ru": "Подробнее",
+        "uz": "Batafsil"
+    },
+    "btn-order" : {
+        "en": "Order Now!",
+        "ru": "Заказать Сейчас!",
+        "uz": "Buyurtma Berish!"
+    },
+    "home-title-2" : {
+        "en": "Fast delivery of goods<br /> to your address",
+        "ru": "Быстрая доставка товара по вашему адресу",
+        "uz": "Tovarlarni manzilingizga tez yetkazib berish"
+    },
+    "farms" : {
+        "en": "Quality Farms",
+        "ru": "Качественные фермы",
+        "uz": "Sifatli fermer xo'jaliklari"
+    },
+    "model" : {
+        "en": "Investment Model",
+        "ru": "Инвестиционная модель",
+        "uz": "Investitsiya modeli"
+    },
+    "goals" : {
+        "en": "Reach your Goals",
+        "ru": "Достигайте своих целей",
+        "uz": "Maqsadlaringizga erishing"
+    },
+    "hiw-card-1" : {
+        "en": "First of all order the product of Milk Star",
+        "ru": "Прежде всего закажите продукт Milk Star",
+        "uz": "Avvalo Milk Star mahsulotiga buyurtma bering"
+    },
+    "hiw-card-2" : {
+        "en": "First of all order the product of Milk Star",
+        "ru": "Прежде всего закажите продукт Milk Star",
+        "uz": "Avvalo Milk Star mahsulotiga buyurtma bering"
+    },
+    "hiw-card-3" : {
+        "en": "First of all order the product of Milk Star",
+        "ru": "Прежде всего закажите продукт Milk Star",
+        "uz": "Avvalo Milk Star mahsulotiga buyurtma bering"
+    },
+    "questions-title" : {
+        "en": "Frequently Asked Questions",
+        "ru": "Часто задаваемые вопросы",
+        "uz": "tez-tez so'raladigan savollar"
+    },
+    "question-1" : {
+        "en": "How is the purchase of goods?",
+        "ru": "Как происходит покупка товара?",
+        "uz": "Tovarlarni sotib olish qanday?"
+    },
+    "question-2" : {
+        "en": "Is there a delivery of the goods?",
+        "ru": "Есть ли доставка товара?",
+        "uz": "Tovarlar etkazib beriladimi?"
+    },
+    "question-3" : {
+        "en": "Is there a loan for the product?",
+        "ru": "Есть ли кредит на продукт?",
+        "uz": "Mahsulot uchun kredit bormi?"
+    },
+    "question-4" : {
+        "en": "Is there a concession if I buy a lot of items?",
+        "ru": "Есть ли скидка, если я куплю много вещей?",
+        "uz": "Ko'p narsalarni sotib olsam, imtiyoz bormi?"
+    },
+    "newsletter" : {
+        "en": "Newsletter",
+        "ru": "Заявка",
+        "uz": "So'rovnoma"
+    },
+    "input-email" : {
+        "en" : "Your email ...",
+        "ru" : "Ваш адрес электронной почты ...",
+        "uz" : "Sizning elektron manzilingiz ..."
+    },
+    "input-name" : {
+        "en" : "Full name ...",
+        "ru" : "ФИО ...",
+        "uz" : "To'liq ism ..."
+    },
+    "call-us" : {
+        "en" : "CALL US",
+        "ru" : "ЗВОНИТЕ НАМ",
+        "uz" : "QO'NG'IROQ QILING"
+    },
+    "email-us" : {
+        "en" : "EMAIL US",
+        "ru" : "ПИШИТЕ НАМ",
+        "uz" : "MUROJAT QILING"
+    },
+    "visit-us" : {
+        "en" : "VISIT US",
+        "ru" : "ПОСЕТИТЬ НАС",
+        "uz" : "BIZGA KELING"
+    },
+    "navigation" : {
+        "en" : "Navigation",
+        "ru" : "Навигация",
+        "uz" : "Navigatsiya"
+    },
+    "home-c" : {
+        "en": "Home",
+        "ru": "Главная",
+        "uz": "Asosiy"
+    },
+    "hiw-c" : {
+        "en": "How it works",
+        "ru": "Как это работает",
+        "uz": "Bu qanday ishlaydi"
+    },
+    "gallery-c" : {
+        "en": "Gallery",
+        "ru": "Галерея",
+        "uz": "Galereya"
+    },
+    "testimonials-c" : {
+        "en": "Testimonials",
+        "ru": "Отзывы",
+        "uz": "Fikrlar"
+    },
+    "join-c" : {
+        "en": "JOIN",
+        "ru": "ПРИСОЕДИНИТЬСЯ",
+        "uz": "QO'SHLISH"
+    },
+    "learn-grow" : {
+        "en": "Learn & Grow",
+        "ru": "Учиться и расти",
+        "uz": "O'rganing va o'sing"
+    },
+    "faq" : {
+        "en": "FAQ",
+        "ru": "Часто задаваемые вопросы",
+        "uz": "TSS"
+    },
+    "career" : {
+        "en": "Career",
+        "ru": "Карьера",
+        "uz": "Karyera"
+    },
+    "privacy-policy" : {
+        "en": "Privacy Policy",
+        "ru": "Политика конфиденциальности",
+        "uz": "Maxfiylik siyosati"
+    },
+    "terms-conditions" : {
+        "en": "Terms Conditions",
+        "ru": "Условия использования",
+        "uz": "Shartlar shartlari"
+    },
+    "reach-us" : {
+        "en": "Reach us",
+        "ru": "Связаться с нами",
+        "uz": "Biz bilan bog'laning"
+    }
+
+    
+}
--- js/script.js
+++ js/script.js
@@ -101,6 +101,39 @@ prev.addEventListener('click', () => {
 })
 
 
-// INITIALIZE AOS
 
-AOS.init();
+// Multi language
+let select = document.querySelector('select'),
+	allLang = ['en', 'ru', 'uz']
+select.addEventListener('change', changeURLLanguage)
+
+// Function that changes url of languages
+function changeURLLanguage() {
+    let lang = select.value
+    location.href = window.location.pathname + '#' + lang
+    location.reload()
+}
+
+function changeLanguage() {
+    let hash = window.location.hash
+    hash = hash.substr(1)
+    console.log(hash)
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en'
+        location.reload()
+    }
+    select.value = hash
+    document.querySelector('title').innerHTML = langArr['unit'][hash]
+	document.getElementsByName('Email')[0].placeholder= langArr['input-email'][hash]
+	document.getElementsByName('Name')[0].placeholder= langArr['input-name'][hash]
+    // document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key)
+        if (elem) {
+            elem.innerHTML = langArr[key][hash]
+        }
+
+    }
+}
+
+changeLanguage()
