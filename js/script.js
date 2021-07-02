34e9abf85a2fdcd1eb17f26fceea9df8ea1c8fcb
Javohir Nurmatjonov <javoxirone@gmail.com>
2021-07-02 23:30:04 +0500

    Blocked scroll while intro

--- js/script.js
+++ js/script.js
@@ -167,10 +167,11 @@ for(let i = 0; i < btn.length; i++){
 // Preloader animation
 
 $(document).ready(function() {
- 
+    $('body').css('overflow', 'hidden');
     setTimeout(function(){
         $('.preloader').addClass('loaded');
         $('.preloader').css('z-index', '-1');
+        $('body').css('overflow', 'scroll');        
     }, 3000);
  
 });
